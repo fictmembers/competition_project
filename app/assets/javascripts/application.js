fa66f17a18f0c8435184a72e92a9e9ff7ea97d3a
costefan <costefan@yandex.ru>
2015-05-10 13:05:41 +0300

    Finished sign in

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,5 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
 //= require materialize-sprockets
-//= require bootstrap-sprockets
+//= require_tree .
--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,16 +1,18 @@
 $(document).ready(function() {
+    $('.slider').slider();
         $("#nav-mobile").on("click", "a", function (event)    //обработка якорей
             {
-                event.preventDefault();     //отменяем стандартную обработку нажатия по ссылке
-                var id = $(this).attr('href');
-                if (id != 'about') {
-                    var top = $(id).offset().top - 45;
-                }
-                else
-                {
-                    var top = $(id).offset().top - 35 - $('.head').height;
-                }
-                $('body,html').animate({scrollTop: top}, 1500);
+                if ($(this).attr('class') != 'sign-in') {
+                    event.preventDefault();     //отменяем стандартную обработку нажатия по ссылке
+                    var id = $(this).attr('href');
+                    if (id != 'about') {
+                        var top = $(id).offset().top - 45;
+                    }
+                    else {
+                        var top = $(id).offset().top - 35 - $('.head').height;
+                    }
+                    $('body,html').animate({scrollTop: top}, 1500);
+                }  
             });
                          });
 
