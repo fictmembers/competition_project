1612397ef596339b363690bf9f68e50ef77c829f
ComandaKOK <rmkaporin@gmail.com>
2015-05-17 00:18:58 +0300

    Skeleton Done!

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     $('.slider').slider();
+    $('.dropdown-button').dropdown();
         $("#nav-mobile").on("click", "a", function (event)    //обработка якорей
             {
                 if ($(this).attr('class') != 'sign-in') {
